0c354832d41dfe7cde138d3f03e69cc84170dc32
habacat <pb_hbc@163.com>
2023-09-11 21:30:42 +0800

    摆烂了

--- src/lib/components/organisms/About.svelte
+++ src/lib/components/organisms/About.svelte
@@ -8,18 +8,55 @@
 	<div class="info">
 		<h2>欢迎关注 复旦电子竞技联盟</h2>
 		<p><SparklingHighlight color="secondary">感谢每一个热爱电竞的你！</SparklingHighlight></p>
-		<p>使用微信扫码二维码加入电竞社核心社群，校内的游戏群聊都在里面哦~</p>
+	</div>
+	<br />
+	<div class="catalog">
+		<br />
+		<h2><SparklingHighlight color="secondary">点击下方链接快速定位</SparklingHighlight></h2>
+		<br />
+		<p>
+			<SparklingHighlight color="primary"><a href="#qrcode">复旦电子竞技联盟AFE官方公众号</a></SparklingHighlight>
+		</p>
+		<!-- <br /> -->
+		<p>
+			<SparklingHighlight color="primary"
+				><a href="#newcomers">2023电竞社核心部门招新</a></SparklingHighlight>
+		</p>
+		<br />
+		<h3><SparklingHighlight color="secondary">电竞社下属群组：</SparklingHighlight></h3>
+		<p>
+			<SparklingHighlight color="primary"><a href="#lol">电竞社LOL总群</a></SparklingHighlight></p>
+			<p><SparklingHighlight color="primary"><a href="#hpjy">和平精英&PUBG ers官群</a></SparklingHighlight></p>
+			<p><SparklingHighlight color="primary"><a href="#jcc">金铲铲之战&云顶之弈</a></SparklingHighlight></p>
+			<p><SparklingHighlight color="primary"><a href="#wzry">复旦王者荣耀冲冲冲</a></SparklingHighlight></p>
+			<p><SparklingHighlight color="primary"><a href="#wwqy">无畏契约（VALORANT）</a></SparklingHighlight></p>
+			<p><SparklingHighlight color="primary"><a href="#NO5">第五人格协会</a></SparklingHighlight></p>
+			<p><SparklingHighlight color="primary"><a href="#apex">APEX总群</a></SparklingHighlight></p>
+			<p><SparklingHighlight color="primary"><a href="#hp">哈利波特：魔法觉醒（微信群）</a></SparklingHighlight></p>
+			<p><SparklingHighlight color="primary"><a href="#cs2">Counter-Strike 2（微信群）</a></SparklingHighlight></p>
+			<p><SparklingHighlight color="primary"><a href="#dota">DOTA2（微信群）</a></SparklingHighlight></p>
+			<p><SparklingHighlight color="primary"><a href="#xjzb">星际争霸--复星社的指挥官们</a></SparklingHighlight></p>
+			<p><SparklingHighlight color="primary"><a href="#dzpd">蛋仔派对</a></SparklingHighlight></p>
+			<!-- <SparklingHighlight color="primary"><a href="#lol">LOL总群</a></SparklingHighlight>
+			<SparklingHighlight color="primary"><a href="#lol">LOL总群</a></SparklingHighlight>
+			<SparklingHighlight color="primary"><a href="#lol">LOL总群</a></SparklingHighlight>
+			<SparklingHighlight color="primary"><a href="#lol">LOL总群</a></SparklingHighlight>
+			<SparklingHighlight color="primary"><a href="#lol">LOL总群</a></SparklingHighlight> -->
+		<!-- </p> -->
+	</div>
+	<div class="info">
+		<p>欢迎扫描二维码加入电竞社核心社群，同时也欢迎加入下列各群，校内的游戏群聊都在里面哦~</p>
 		<p />
-		<div class="socials">
+		<!-- <div class="socials">
 			<!-- <span>Socials:</span> -->
 			<!-- <Socials /> -->
-		</div>
+		<!-- </div> -->
 	</div>
 	<div class="image">
-		<img src="/images/qrcode.webp" alt="logo" />
+		<div class="qrcode"></div><img src="/images/qrcode.webp" alt="logo" />
 		<p />
 		⁣⁣⁣⁣　⁣⁣⁣⁣　⁣⁣⁣
-		<img src="/images/sample-image.webp" alt="logo" />
+		<name class="newcomers"></name><img src="/images/sample-image.webp" alt="logo" />
 	</div>
 	<p />
 	<!-- <h2>复旦大学电子竞技联盟微信公众号</h2> -->
@@ -29,30 +66,44 @@
 	<h2>电竞社下属QQ群组</h2>
 	<br />
 	<div class="image2">
-		<img src="/images/group_webp/lol总群.webp" alt="logo" />
+		<div id="lol"></div><img src="/images/group_webp/lol总群.webp" alt="logo" />
 		<p />
 		⁣⁣⁣⁣　⁣⁣⁣⁣　⁣⁣⁣
-		<img src="/images/group_webp/fdu和平精英.webp" alt="logo" />
+		<div id="hpjy"></div><img src="/images/group_webp/fdu和平精英.webp" alt="logo" />
 		<p />
 		⁣⁣⁣⁣　⁣⁣⁣⁣　⁣⁣⁣
-		<img src="/images/group_webp/fdu金铲铲.webp" alt="logo" />
+		<div id="jcc"></div><img src="/images/group_webp/fdu金铲铲.webp" alt="logo" />
 		<p />
 		⁣⁣⁣⁣　⁣⁣⁣⁣　⁣⁣⁣
-		<img src="/images/group_webp/复旦王者荣耀.webp" alt="logo" />
+		<div id="wzry"></div><img src="/images/group_webp/复旦王者荣耀.webp" alt="logo" />
 	</div>
 	<br />
 	<br />
 	<div class="image2">
-		<img src="/images/group_webp/fdu无畏契约.webp" alt="logo" />
+		<div id="wwqy"></div><img src="/images/group_webp/fdu无畏契约.webp" alt="logo" />
 		<p />
 		⁣⁣⁣⁣　⁣⁣⁣⁣　⁣⁣⁣
-		<img src="/images/group_webp/哈利波特魔法觉醒.webp" alt="logo" />
+		<div id="hp"></div><img src="/images/group_webp/哈利波特魔法觉醒.webp" alt="logo" />
 		<p />
 		⁣⁣⁣⁣　⁣⁣⁣⁣　⁣⁣⁣
-		<img src="/images/group_webp/cs2游戏群.webp" alt="logo" />
+		<div id="cs2"></div><img src="/images/group_webp/cs2游戏群.webp" alt="logo" />
 		<p />
 		⁣⁣⁣⁣　⁣⁣⁣⁣　⁣⁣⁣
-		<img src="/images/group_webp/复旦大学第五人格.webp" alt="logo" />
+		<div id="NO5"></div><img src="/images/group_webp/复旦大学第五人格.webp" alt="logo" />
+	</div>
+	<br />
+	<br />
+	<div class="image2">
+		<div id="dota"></div><img src="/images/group_webp/dota2微信.webp" alt="logo" />
+		<p />
+		⁣⁣⁣⁣　⁣⁣⁣⁣　⁣⁣⁣
+		<div id="apex"></div><img src="/images/group_webp/fdu_apex.webp" alt="logo" />
+		<p />
+		⁣⁣⁣⁣　⁣⁣⁣⁣　⁣⁣⁣
+		<div id="xjzb"></div><img src="/images/group_webp/fdu_star.webp" alt="logo" />
+		<p />
+		⁣⁣⁣⁣　⁣⁣⁣⁣　⁣⁣⁣
+		<div id="dzpd"></div><img src="/images/group_webp/fdu蛋仔派对.webp" alt="logo" />
 	</div>
 </section>
 
@@ -60,6 +111,8 @@
 	@import '$lib/scss/breakpoints.scss';
 
 	#about {
+		width: auto;
+		height: auto;
 		position: relative;
 		grid-template-columns: 500px 250px;
 		align-items: center;
@@ -107,19 +160,55 @@
 			}
 		}
 
-		.socials {
+		.catalog {
 			display: flex;
 			align-items: center;
 			gap: 15px;
-
+			display: none;
 			@include for-phone-only {
 				justify-content: center;
 				margin-bottom: 10px;
-				span {
-					display: none;
+				display: block;
+				text-align: center;
+			}
+			p{
+				line-height: 190%;
+			}
+			h2 {
+				@include for-phone-only {
+					text-align: center;
+					font-family: sans-serif;
+					display: flex;
+					flex-direction: column;
+					font-size: 1.6rem;
+				}
+			}
+			h3 {
+				@include for-phone-only {
+					text-align: center;
+					font-family: sans-serif;
+					display: flex;
+					flex-direction: column;
+					font-size: 1.2rem;
+					font-style: italic;
+					font-weight: 40;
 				}
 			}
 		}
+
+		// .socials {
+		// 	display: flex;
+		// 	align-items: center;
+		// 	gap: 15px;
+
+		// 	@include for-phone-only {
+		// 		justify-content: center;
+		// 		margin-bottom: 10px;
+		// 		span {
+		// 			display: none;
+		// 		}
+		// 	}
+		// }
 
 		.image {
 			width: 300px;
@@ -128,12 +217,15 @@
 			justify-content: center; /* 居中对齐图片 */
 			margin: auto;
 			@include for-phone-only {
+				width: 80%;
+				height: 80%;
+				margin: 10%;
 				justify-content: center;
-				margin-bottom: 100px;
-				margin-top: 120px;
+				// margin-bottom: 100px;
+				// margin-top: 160px;
 				// margin-left: 10px;
 				// margin-right: 10px;
-				margin-inline-end: auto;
+				// margin-inline-end: auto;
 				// display: none;
 				flex-direction: column;
 			}
@@ -147,7 +239,9 @@
 			@include for-phone-only {
 				width: auto;
 				height: auto;
-				margin: auto;
+				// margin: 10%;
+				// margin-left: 10%;
+				// margin-right: 10%;
 				// justify-content: center;
 				margin-bottom: auto;
 				margin-top: auto;
